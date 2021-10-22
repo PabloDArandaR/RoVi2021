6997bd685328b59872085dd790c1e1e9c2914f94
PabloDavidAR <pablo.arandarod@gmail.com>
2021-10-22 19:42:41 +0200

    Organization of files modified

---
+++ src/mainvis.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <rw/rw.hpp>
+#include <rw/loaders/WorkCellLoader.hpp>
+#include <rw/models/WorkCell.hpp>
+
+int main()
+{
+    std::string scenePath("../../../ProjectWorkCell/Scene.wc.xml");
+
+
+
+    return 0;
+}
