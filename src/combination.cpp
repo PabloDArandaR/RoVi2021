c9a20571dd1c0b64e1e526b03b0e4cbbd827415c
PabloDavidAR <pablo.arandarod@gmail.com>
2021-11-20 16:25:17 +0100

    Both

---
+++ src/combination.cpp
@@ -0,0 +1,15 @@
+int main()
+{
+    // Detect the object
+
+    // Find the pose
+
+    // Obtain the frame of the object based on the frame of the camera and the pose detected
+
+    // Use the frame obtained to choose the first intermediate point of the path.
+
+    // 1 intermediate point more
+
+    // end-approximation points
+    return 0;
+}
