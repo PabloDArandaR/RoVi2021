402ea76980b8cbb5ca99fd3beb7fbd1eb3d59b6e
joffrey011 <dolyjoffrey@gmail.com>
2021-12-06 13:06:48 +0100

    Add files via upload

--- src/vision/pcl_processing.cpp
+++ src/vision/pcl_processing.cpp
@@ -1,21 +1,26 @@
 #include <iostream>
 #include <pcl/io/pcd_io.h>
 #include <pcl/point_types.h>
+
 #include <pcl/filters/voxel_grid.h>
 #include <pcl/filters/statistical_outlier_removal.h>
 #include <pcl/filters/convolution_3d.h>
 #include <pcl/kdtree/kdtree_flann.h>
 #include <pcl/surface/mls.h>
 #include <pcl/filters/passthrough.h>
+#include <pcl/common/transforms.h>
+
 #include <pcl/visualization/cloud_viewer.h>
 
 
-void voxelGrid( pcl::PointCloud<pcl::PointXYZRGB>::Ptr input_cloud, pcl::PointCloud<pcl::PointXYZRGB>::Ptr &output_cloud )
+
+
+void voxelGrid( pcl::PointCloud<pcl::PointXYZRGB>::Ptr input_cloud, pcl::PointCloud<pcl::PointXYZRGB>::Ptr &output_cloud, float leafSize)
 {
   // Create the filtering object
   pcl::VoxelGrid<pcl::PointXYZRGB> sor;
   sor.setInputCloud (input_cloud);
-  sor.setLeafSize (1.0f, 1.0f, 1.0f);
+  sor.setLeafSize (leafSize, leafSize, leafSize);
   sor.filter (*output_cloud);
 	
 }
@@ -40,7 +45,7 @@ void spatialFilter( pcl::PointCloud<pcl::PointXYZRGB>::Ptr input_cloud, pcl::Poi
   pcl::PassThrough<pcl::PointXYZRGB> pass;
   pass.setInputCloud (input_cloud);
   pass.setFilterFieldName ("z");
-  pass.setFilterLimits (260.0, 320.0);
+  pass.setFilterLimits (-1.310, 2);
   pass.filter (*output_cloud);
 }
 
@@ -75,14 +80,31 @@ void smoothing( pcl::PointCloud<pcl::PointXYZRGB>::Ptr input_cloud, pcl::PointCl
 int main (int argc, char** argv)
 {
   pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZRGB>);
-  pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_filtered (new pcl::PointCloud<pcl::PointXYZRGB>);
-
+  pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_voxeled(new pcl::PointCloud<pcl::PointXYZRGB>);
+  pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_transformed (new pcl::PointCloud<pcl::PointXYZRGB> ());
+  pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_spatialized (new pcl::PointCloud<pcl::PointXYZRGB>);
+    
   // Fill in the cloud data
   pcl::PCDReader reader;
   reader.read ("../../../RoviSamplePlugin/Scanner25D.pcd", *cloud); // Remember to download the file first!
+  
+  voxelGrid(cloud,cloud_voxeled, 0.008);
+  
+  Eigen::Affine3f transform_2 = Eigen::Affine3f::Identity();
+  transform_2.rotate (Eigen::AngleAxisf (-(3.1415*25.0)/180.0, Eigen::Vector3f::UnitX()));
 
-    pcl::visualization::CloudViewer viewer ("Simple Cloud Viewer");
- viewer.showCloud (cloud);
+
+  
+  // You can either apply transform_1 or transform_2; they are the same
+  pcl::transformPointCloud (*cloud_voxeled, *cloud_transformed, transform_2);
+
+  spatialFilter(cloud_transformed,cloud_spatialized);
+  
+  int32_t rgb = (static_cast<uint32_t>(255) << 16 | static_cast<uint32_t>(255) << 8 | static_cast<uint32_t>(255));
+    for(auto &p: cloud_spatialized->points) p.rgb=rgb;
+
+  pcl::visualization::CloudViewer viewer("Simple Cloud Viewer");
+ viewer.showCloud (cloud_spatialized);
 while (!viewer.wasStopped ())
 {
 }
